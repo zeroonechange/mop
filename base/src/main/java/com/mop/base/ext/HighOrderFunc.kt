f7929473e7107e8a68f6c308d292a41a36475d4e
Money Orientation Programming <zeroonechange@gmail.com>
2022-03-25 13:48:15 +0800

    委托属性

--- base/src/main/java/com/mop/base/ext/HighOrderFunc.kt
+++ base/src/main/java/com/mop/base/ext/HighOrderFunc.kt
@@ -5,7 +5,7 @@ import kotlin.reflect.KProperty
 
 class HighOrderFunc {
     //  在另一个module 就无法使用了
-    internal fun getSome(): String{
+    internal fun getSome(): String {
         return "internal"
     }
 }
@@ -26,7 +26,7 @@ class SingleIns {
 
 class SingleIns2 {
 
-    companion object{
+    companion object {
         @Volatile
         private var instance: SingleIns2? = null
     }
@@ -64,7 +64,8 @@ class MyLateinit {
         }
     }
 }
-// SAM    只有一个抽象方法的接口  单一抽象方法 single abstract method
+
+// SAM    只有一个抽象方法的接口  单一抽象方法 single abstract method   回调的时候 用这个 比较好
 fun interface Intercepeter {
     fun accept(i: Int): Boolean
 }
@@ -98,7 +99,9 @@ typealias FileTable<K> = MutableMap<K, MutableList<File>> // 常类型 别名
 typealias MyHandler = (Int, String, Any) -> Unit   //函数别名
 typealias Predicate<T> = (T) -> Boolean   // 函数 泛型别名
 
-class Network { class Node{} }
+class Network {
+    class Node {}
+}
 
 
 typealias NameTypeAlias = String
@@ -134,17 +137,19 @@ fun ab() {
 }
 
 // 扩展函数
-fun MutableList<Int>.swap1(id1: Int, id2: Int){
+fun MutableList<Int>.swap1(id1: Int, id2: Int) {
     val tmp = this[id1]
     this[id1] = this[id2]
     this[id2] = tmp
 }
-fun ac(){
+
+fun ac() {
     val list = mutableListOf(1, 2, 3)
     list.swap1(0, 2)
     list.swap(0, 2)
 }
-fun <T> MutableList<T>.swap(id1:Int, id2: Int){
+
+fun <T> MutableList<T>.swap(id1: Int, id2: Int) {
     val tmp = this[id1]
     this[id1] = this[id2]
     this[id2] = tmp
@@ -154,27 +159,33 @@ fun <T> MutableList<T>.swap(id1:Int, id2: Int){
 fun maina() {
     open class Shape
     class Rect : Shape()
-    fun Shape.getname()="shape"
-    fun Rect.getname()="rect"
-    fun printClsName(s: Shape){ println(s.getname())}
+
+    fun Shape.getname() = "shape"
+    fun Rect.getname() = "rect"
+    fun printClsName(s: Shape) {
+        println(s.getname())
+    }
     printClsName(Rect())
 }
+
 // 扩展 属性
 val <T> List<T>.lastIdx: Int
     get() = size - 1
 
 // 伴生对象  类内部的对象声明 companion
-class Myclass{
-    companion object Factory{
+class Myclass {
+    companion object Factory {
         fun create(): Myclass = Myclass()
     }
 }
-class MyClass{
-    companion object{ }
+
+class MyClass {
+    companion object {}
 }
+
 // 看起来像其他语言的静态成员  在运行时 仍然时真实对象的实例对象成员 还能实现接口
 // 使用 @JvmStatic 注解  可以将伴生对象的成员生成为真正的静态方法和字段
-fun mainb(){
+fun mainb() {
     val instance = Myclass.create() //
     val x = MyClass.Companion
 }
@@ -190,44 +201,92 @@ fun mainb(){
 class example {
     var p: String by Delegate()   //语法是  val/var <属性名>: <属性类型> by <表达式>
 }
-class Delegate{
-    operator fun getValue(thisRef: Any?, property: KProperty<*>):String{
+
+class Delegate {
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): String {
         return "$thisRef, tks for delegate '${property.name}' to me"
     }
+
     operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
-        println( "$value has been assigned to '${property.name}'.")
+        println("$value has been assigned to '${property.name}'.")
     }
 }
+
 fun mainc() {
     val ex = example()
     println(ex.p)
     ex.p = "*****"
     println(ex.p)
 }
+
 //标准委托  lazy   默认同步锁-synchronized  线程安全的话 可以不加锁
-val lazyValue : String by lazy(LazyThreadSafetyMode.NONE) {
+val lazyValue: String by lazy(LazyThreadSafetyMode.NONE) {
     println("computed!")
     "hello"
 }
+
 fun mainv() {
     println(lazyValue)
     println(lazyValue)
 }
+
 // 可观察属性
-class User{
-    var name: String by Delegates.observable("<no name>"){
-        property, oldValue, newValue ->
+class User {
+    var name: String by Delegates.observable("<no name>") { property, oldValue, newValue ->
         println("$oldValue -> $newValue")
     }
 }
 
-fun main() {
-
+fun mainav() {
     val user = User()
     user.name = "first"
     user.name = "second"
-
 }
+
+//委托给另一个属性
+var top: Int = 0
+
+class withDelegate(val another: Int)
+class MyCls(var member: Int, val anotherIns: withDelegate) {
+    var delegateMember: Int by this::member
+    var delegateTop: Int by ::top
+    val delegateAnother: Int by anotherIns::another
+}
+var MyCls.extDelegated: Int by ::top
+var MyCls.outDelegate: Int by ::top
+
+// 向后兼容 重命名一个属性 注解旧的属性 委托实现
+class MyCls2 {
+    var newName: Int = 0
+    @Deprecated("use 'newName' instead ", ReplaceWith("newName"))
+    var oldName: Int by this::newName
+}
+fun mainab() {
+    var cls = MyCls2()
+    cls.oldName = 33
+    println(cls.newName)
+    println(cls.oldName)
+}
+// 类似与 json解析   委托实现属性映射
+class User1(val map: Map<String, Any?>){
+    val name: String by map
+    val age : Int by map
+}
+
+class User2(var map: MutableMap<String, Any?>){
+    val name: String by map
+    val age : Int by map
+}
+
+fun mainam() {
+//    val user = User1(mapOf("name" to "John", "age" to 22))
+    val user = User2(mutableMapOf("name" to "John", "age" to 22))
+    println(user.name)
+    println(user.age)
+}
+
+// 局部委托属性
+
 
 
 
